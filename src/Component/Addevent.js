baf95c7a58bb89691afd2aea347f0f0e3c228069
Kavyakrishnan <kavyakrishnan9821@gmail.com>
2022-05-25 17:16:43 +0530

    complete

---
+++ src/Component/Addevent.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Addevent = () => {
+  return (
+    <div><div className="Container">
+    <div className="row g-2">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Name</label>
+                    <input  type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Date</label>
+                    <input  type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                     <label for="" className="form-label">Venue</label>
+                    <input  type="text" className="form-control"/></div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Organiser</label>
+                    <input  type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Contact Number</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button  className="btn btn-success">Register</button>
+                </div>
+              
+            </div>
+        </div>
+    </div>
+</div></div>
+  )
+}
+
+export default Addevent
